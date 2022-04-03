514d42dc25bec3ee3e7f44b424ad62b686c77f0b
Fikri-IF <ihsanfikri00@gmail.com>
2022-04-03 13:08:01 +0700

    revisi

--- src/15_puzzle.py
+++ src/15_puzzle.py
@@ -1,22 +1,27 @@
 import numpy as np
 from copy import deepcopy
 import time
-import sys
 
 def is_have_solution(arr_for_checking,black_tile):
+    '''
+    Mengecek apakah puzzle memiliki penyelesaian atau tidak berdasarkan fungsi KURANG(i)
+    '''
     que=[0 for i in range (16)]
     sum=0
     for i in range(np.size(arr_for_checking)):
-        #component=0
         if arr_for_checking[i]==16 and i in black_tile:
             sum+=1
         for j in range(i+1,np.size(arr_for_checking)):
             if arr_for_checking[i]>arr_for_checking[j]:
                 sum+=1
-                que[arr_for_checking[i]-1]+=1
+                que[arr_for_checking[i]-1]+=1 #Untuk Keperluan display fungsi KURANG(i) dari setiap ubin tidak kosong
+    
+    #Keperluan Display
     for i in range(16):
         print("Nilai fungsi Kurang("+str(i+1)+") = " + str(que[i]))
     print("\nTotal nilai Fungsi KURANG(i) + X adalah " + str(sum)+"\n")
+    #Keperluan Display
+
     if sum%2==0:
         return True
     else:
@@ -31,30 +36,32 @@ def display_matrix(matrix):
                 print(str(matrix[i][j])+"\t",end="")
         print("")
 
+#Gerak kekanan
 def move_right(state,x,y):
     temp = state[x][y]
     state[x][y]=state[x][y+1]
     state[x][y+1]=temp
     return state
-    
+#Gerak keatas
 def move_up(state,x,y):
     temp = state[x][y]
     state[x][y]=state[x-1][y]
     state[x-1][y]=temp
     return state
-
+#Gerak kekiri
 def move_left(state,x,y):
     temp = state[x][y]
     state[x][y]=state[x][y-1]
     state[x][y-1]=temp
     return state
-
+#Gerak kebawah
 def move_down(state,x,y):
     temp = state[x][y]
     state[x][y]=state[x+1][y]
     state[x+1][y]=temp
     return state
 
+#Mendapatkan letak ubin kosong
 def get_blank_location(arr):
     for i in range(4):
         for j in range(4):
@@ -64,6 +71,7 @@ def get_blank_location(arr):
                 break
     return x,y
 
+#Untuk menghitung cost dari sebuah simpul
 def count_cost(arr):
     cost=0
     arr_for_checking=np.ravel(arr)
@@ -72,18 +80,27 @@ def count_cost(arr):
             cost+=1
     return cost
 
+#Keperluan sorting simpul hidup
 def get_cost(node_):
     return node_.cost,node_.depth
-
+#Keperluan sorting simpul hidup
 def ins_to_que(que_,node_):
     que_.append(node_)
     que_.sort(key=get_cost) #Mengurutkan simpul hidup berdasarkan cost lalu jika sama, berdasarkan kedalaman simpul
 
+#Menyisipkan simpul simpul yang terbentuk dari setiap pergerakan kedalam simpul hidup
 def move(que_,moved,node_):
+    '''
+    que_ adalah list simpul hidup
+    moved adalah simpul denga bentuk matrix setelah bergerak
+    node_ adalah simpul denga bentuk matrix sebelum bergerak
+    
+    '''
     x,y=get_blank_location(moved)
-    moved_node = node(moved,node_,node_.depth+1,x,y,count_cost(moved)+node_.depth+1)
+    moved_node = node(moved,node_,node_.depth+1,x,y,count_cost(moved)+node_.depth+1) #Membentuk object simpul
     ins_to_que(que_,moved_node)
 
+#Untuk cek apakah sebuah bentuk sudah pernah tercapai atau belum
 def visited_or_not(visited,moved):
     i=0
     ada=False
@@ -93,15 +110,16 @@ def visited_or_not(visited,moved):
         i+=1
     return ada
 
+#Pergerakan dari setiap node
 def solve(sol_,que_,node_,visited):
     next_node=node_
-    global node_generated
+    global node_generated #Untuk menghitung jumlah simpul yang dibangkitkan
     while not(np.array_equal(goal_state,next_node.state)): #Hingga menemukan hasil akhir
         if next_node.x != 0:
             moved = move_up(deepcopy(next_node.state),next_node.x,next_node.y)
             if not(visited_or_not(visited,moved)): #Jika susunan puzzle belum pernah dijumpai maka, dimasukkan ke dalam simpul hidup
                 move(que_,moved,next_node)
-                node_generated+=1
+                node_generated+=1 #Jumlah simpul yang dibangkitkan
                 visited.append(moved) #Me-list semua susunan puzzle yang pernah dijumpai
         if next_node.y != 3:
             moved = move_right(deepcopy(next_node.state),next_node.x,next_node.y)
@@ -122,11 +140,12 @@ def solve(sol_,que_,node_,visited):
                 node_generated+=1
                 visited.append(moved)
         next_node=que_.pop(0) #Mengambil node dengan cost terkecil
-    sol_.append(next_node) 
+    sol_.append(next_node) #Menambahkan solusi
     que_.clear() #Menghapus semua antrian simpul hidup
 
+#Menampilkan urutan penyelesaian
 def display_path(node_):
-    if node_.parents_node != None:
+    if node_.parents_node != None: #Apabila parents node masih memiliki parents maka rekursifkan
         display_path(node_.parents_node)
         display_matrix(node_.state)
         print("\n")
@@ -134,6 +153,7 @@ def display_path(node_):
         display_matrix(node_.state)
         print("\n")
 
+#Convert txt menjadi matrix
 def teks_to_matriks(_inputfile):
     _case = []
     with open(_inputfile) as file:
@@ -178,7 +198,7 @@ if __name__ == '__main__':
         visited=[] #List simpul yang pernah dikunjungi
         x_start,y_start=get_blank_location(back_to_2d)
         start_node = node(back_to_2d,None,0,x_start,y_start,99)
-        urutan.append(start_node)
+        urutan.append(start_node) #Menambahkan simpul dengan matrix bentuk awal kedalam simpul hidup
         #initiate root ---------------------------
 
         #Runtime-----------------------
